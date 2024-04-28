ed7b70898fa8e78961fde9e63fbb46008cad566c
maross3 <maross334@gmail.com>
2024-04-28 19:35:07 -0400

    controller stuff

--- Assets/Scripts/_dev/Hands/HandController.cs
+++ Assets/Scripts/_dev/Hands/HandController.cs
@@ -37,17 +37,15 @@ public class HandController : MonoBehaviour
         {
             _animator.SetInteger(_SState, 1);
             _ball.GetComponent<DodgeBall>().SetOwner(_controller);
-            var rb = _ball.GetComponent<Rigidbody>();
-            if (!rb.isKinematic) rb.velocity = Vector3.zero;
+            _ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
             _grabbing = true;
         }
-
         _grip = true;
     }
 
     private void SetGrabReleased()
     {
-        if (_grabbing) _ball.GetComponent<DodgeBall>().SetLiveBall();
+        if (_ball && _grabbing) _ball.GetComponent<DodgeBall>().SetLiveBall();
         _animator.SetInteger(_SState, 2);
         _ball = null;
         _grip = false;
@@ -62,8 +60,7 @@ public class HandController : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (!_grabbing && other.gameObject.layer == _ballLayer)
-            _ball = null;
+        if (other.gameObject.layer == _ballLayer) _ball = null;
     }
 
     private void LateUpdate()
