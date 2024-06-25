490e6b672d6189ca01ddca89a3effdd62e5198f2
maross3 <maross334@gmail.com>
2024-06-25 18:37:36 -0400

    Script for callback

---
+++ Assets/Scripts/Multiplayer/ScriptUsageProgrammerSounds.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+class ScriptUsageProgrammerSounds : MonoBehaviour
+{
+    FMOD.Studio.EVENT_CALLBACK dialogueCallback;
+
+    public FMODUnity.EventReference EventName;
+
+#if UNITY_EDITOR
+    void Reset()
+    {
+        EventName = FMODUnity.EventReference.Find("event:/Test_VC2");
+    }
+#endif
+
+    void Start()
+    {
+        dialogueCallback = DialogueEventCallback;
+    }
+
+    void PlayDialogue(string key)
+    {
+        var dialogueInstance = FMODUnity.RuntimeManager.CreateInstance(EventName);
+
+        // Pin the key string in memory and pass a pointer through the user data
+        GCHandle stringHandle = GCHandle.Alloc(key);
+        dialogueInstance.setUserData(GCHandle.ToIntPtr(stringHandle));
+
+        dialogueInstance.setCallback(dialogueCallback);
+        dialogueInstance.start();
+        dialogueInstance.release();
+    }
+
+    [AOT.MonoPInvokeCallback(typeof(FMOD.Studio.EVENT_CALLBACK))]
+    static FMOD.RESULT DialogueEventCallback(FMOD.Studio.EVENT_CALLBACK_TYPE type, IntPtr instancePtr, IntPtr parameterPtr)
+    {
+        FMOD.Studio.EventInstance instance = new FMOD.Studio.EventInstance(instancePtr);
+
+        // Retrieve the user data
+        IntPtr stringPtr;
+        instance.getUserData(out stringPtr);
+
+        // Get the string object
+        GCHandle stringHandle = GCHandle.FromIntPtr(stringPtr);
+        String key = stringHandle.Target as String;
+
+        switch (type)
+        {
+            case FMOD.Studio.EVENT_CALLBACK_TYPE.CREATE_PROGRAMMER_SOUND:
+            {
+                FMOD.MODE soundMode = FMOD.MODE.LOOP_NORMAL | FMOD.MODE.CREATECOMPRESSEDSAMPLE | FMOD.MODE.NONBLOCKING;
+                var parameter = (FMOD.Studio.PROGRAMMER_SOUND_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.PROGRAMMER_SOUND_PROPERTIES));
+
+                if (key.Contains("."))
+                {
+                    FMOD.Sound dialogueSound;
+                    var soundResult = FMODUnity.RuntimeManager.CoreSystem.createSound(Application.streamingAssetsPath + "/" + key, soundMode, out dialogueSound);
+                    if (soundResult == FMOD.RESULT.OK)
+                    {
+                        parameter.sound = dialogueSound.handle;
+                        parameter.subsoundIndex = -1;
+                        Marshal.StructureToPtr(parameter, parameterPtr, false);
+                    }
+                }
+                else
+                {
+                    FMOD.Studio.SOUND_INFO dialogueSoundInfo;
+                    var keyResult = FMODUnity.RuntimeManager.StudioSystem.getSoundInfo(key, out dialogueSoundInfo);
+                    if (keyResult != FMOD.RESULT.OK)
+                    {
+                        break;
+                    }
+                    FMOD.Sound dialogueSound;
+                    var soundResult = FMODUnity.RuntimeManager.CoreSystem.createSound(dialogueSoundInfo.name_or_data, soundMode | dialogueSoundInfo.mode, ref dialogueSoundInfo.exinfo, out dialogueSound);
+                    if (soundResult == FMOD.RESULT.OK)
+                    {
+                        parameter.sound = dialogueSound.handle;
+                        parameter.subsoundIndex = dialogueSoundInfo.subsoundindex;
+                        Marshal.StructureToPtr(parameter, parameterPtr, false);
+                    }
+                }
+                break;
+            }
+            case FMOD.Studio.EVENT_CALLBACK_TYPE.DESTROY_PROGRAMMER_SOUND:
+            {
+                var parameter = (FMOD.Studio.PROGRAMMER_SOUND_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.PROGRAMMER_SOUND_PROPERTIES));
+                var sound = new FMOD.Sound(parameter.sound);
+                sound.release();
+
+                break;
+            }
+            case FMOD.Studio.EVENT_CALLBACK_TYPE.DESTROYED:
+            {
+                // Now the event has been destroyed, unpin the string memory so it can be garbage collected
+                stringHandle.Free();
+
+                break;
+            }
+        }
+        return FMOD.RESULT.OK;
+    }
+
+    void Update()
+    {
+            PlayDialogue("Test_VC2");
+    }
+
+    private void OnDestroy()
+    {
+        dialogueCallback = null;
+    }
+}
