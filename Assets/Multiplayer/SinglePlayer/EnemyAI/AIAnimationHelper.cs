be766816e8f5b30a43fae3afdc12771596f671dd
maross3 <maross334@gmail.com>
2024-08-11 11:42:34 -0400

    Shadowstep ai helper

--- Assets/Multiplayer/SinglePlayer/EnemyAI/AIAnimationHelper.cs
+++ Assets/Multiplayer/SinglePlayer/EnemyAI/AIAnimationHelper.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class AIAnimationHelper : MonoBehaviour
 {
     [SerializeField] private DodgeballAI dodgeballAI;
+    [SerializeField] private GameObject particleEffect;
     // Start is called before the first frame update
     public void Throw()
     {
         dodgeballAI.ThrowBall();
+    }
+    
+    public void ShadowStep()
+    {
+        particleEffect.SetActive(true);
     }
 
     // Update is called once per frame
