74312c2c6c581ad0ef4846713394d7e68920bf1f
maross3 <maross334@gmail.com>
2024-08-12 22:15:13 -0400

    Project update/resolution

--- Assets/Plugins/RootMotion/FinalIK/Ragdoll Utility/RagdollUtility.cs
+++ Assets/Plugins/RootMotion/FinalIK/Ragdoll Utility/RagdollUtility.cs
@@ -297,7 +297,7 @@ namespace RootMotion.FinalIK {
 
 		public void LateUpdate() {
 			// When Mecanim has animated...
-			if (animator.updateMode != AnimatorUpdateMode.Fixed || (animator.updateMode == AnimatorUpdateMode.Fixed && fixedFrame)) {
+			if (animator.updateMode != AnimatorUpdateMode.AnimatePhysics || (animator.updateMode == AnimatorUpdateMode.AnimatePhysics && fixedFrame)) {
 				AfterAnimation();
 			}
 
@@ -348,7 +348,7 @@ namespace RootMotion.FinalIK {
 			}
 			// Switch Animator update mode to AnimatePhysics, so IK is updated in the fixed time step
 			animatorUpdateMode = animator.updateMode;
-			animator.updateMode = AnimatorUpdateMode.Fixed;
+			animator.updateMode = AnimatorUpdateMode.AnimatePhysics;
 
 			// Disable the Animator so it won't overwrite physics
 			if (animator.enabled)
