912837d1c6f289822b8f58c7b6ef86a08c1b30f9
maross3 <maross334@gmail.com>
2024-08-12 21:01:49 -0400

    code restructuring

---
+++ Assets/Scripts/HandControls/HandController.cs
@@ -0,0 +1,139 @@
+using Unity.Template.VR.Multiplayer;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Hands
+{
+    public class HandController : MonoBehaviour
+    {
+        [SerializeField] private Transform grabTransform;
+        [SerializeField] private InputActionAsset actionAsset;
+        [SerializeField] internal HandSide handSide;
+        [SerializeField] private float suckSnapDistance = 0.4f;
+        [SerializeField] private float suckSnapSpeed = 5f;
+        [SerializeField] private GameObject fxPrefab;
+        private LayerMask _ballLayer;
+        private GameObject _ball;
+        private bool _grabbing;
+        private InputAction _gripAction;
+        private Animator _animator;
+
+        private static readonly int _SState = Animator.StringToHash("State");
+
+        // had to serialize it because the component for colliders needed to move, which is not parented
+        [SerializeField] private DevController controller;
+
+
+        public bool networked;
+
+
+        private void Awake()
+        {
+            return;
+            _animator = GetComponentInChildren<Animator>();
+            _ballLayer = LayerMask.NameToLayer("Ball");
+
+            _gripAction =
+                actionAsset.FindAction(
+                    handSide == HandSide.RIGHT ? "XRI RightHand Interaction/Select" : "XRI LeftHand Interaction/Select",
+                    true);
+        }
+
+        private void OnEnable()
+        {
+            _gripAction.performed += SetGrab;
+            _gripAction.canceled += SetGrabReleased;
+        }
+
+        #region single player grabs
+
+        private int _ballIndex;
+
+        private void SetGrab(InputAction.CallbackContext e)
+        {
+            if (controller.IsOutOfPlay()) return;
+            
+            fxPrefab.SetActive(true);
+            if (_ball && _ball.layer != _ballLayer) return;
+            if (_ball && !_grabbing)
+            {
+                controller.hasBall = true;
+                _animator.SetInteger(_SState, 1);
+
+                var dodgeBall = _ball.GetComponent<DodgeBall>();
+                // dodgeBall.SetOwner(controller.team);
+                dodgeBall.SetParticleActive(false);
+                var rb = _ball.GetComponent<Rigidbody>();
+                if (!rb.isKinematic) rb.velocity = Vector3.zero;
+                _grabbing = true;
+                var throwHandle = _ball.GetComponent<ThrowHandle>();
+                throwHandle.OnAttach(gameObject, gameObject);
+            }
+        }
+
+        private void SetGrabReleased(InputAction.CallbackContext e)
+        {
+            fxPrefab.SetActive(false);
+            if (_grabbing)
+            {
+                var dodgeBall = _ball.GetComponent<DodgeBall>();
+                dodgeBall.SetLiveBall();
+                dodgeBall.SetParticleActive(true);
+                _ball.GetComponent<ThrowHandle>().OnDetach();
+                controller.hasBall = false;
+
+                // this is set after ownership on net
+                if (!networked) _ball = null;
+            }
+
+            // this is set after ownership on net
+            if (!networked) _grabbing = false;
+            _animator.SetInteger(_SState, 2);
+        }
+
+        #endregion
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!_ball && 
+                (!_grabbing || fxPrefab.activeInHierarchy) && 
+                other.gameObject.layer == _ballLayer)
+            {
+                _ball = other.gameObject;
+            }
+            
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!_grabbing && other.gameObject.layer == _ballLayer)
+                _ball = null;
+        }
+
+        private void Update()
+        {
+            
+            if (!_grabbing || !_ball || !fxPrefab.activeInHierarchy) return;
+            if (fxPrefab.activeInHierarchy &&
+                Vector3.Distance(_ball.transform.position, grabTransform.position) > suckSnapDistance)
+            {
+                _ball.transform.position = Vector3.Lerp(_ball.transform.position, grabTransform.position,
+                    Time.fixedDeltaTime * suckSnapSpeed);
+                _ball.transform.rotation = Quaternion.Lerp(_ball.transform.rotation, grabTransform.rotation,
+                    Time.fixedDeltaTime * suckSnapSpeed);
+            }
+            else
+            {
+                SetGrab(default);
+                fxPrefab.SetActive(false);
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (!_ball || !_grabbing) return;
+            _ball.transform.position = grabTransform.position;
+            _ball.transform.rotation = grabTransform.rotation;
+        }
+    }
+}
---
+++ Assets/Scripts/HandControls/NetBallPossessionHandler.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class DodgeballIndex
+{
+    public BallType type;
+    public GameObject dodgeball;
+}
+[RequireComponent(typeof(Animator))]
+public class NetBallPossessionHandler : MonoBehaviour
+{
+    [SerializeField] private List<DodgeballIndex> dodgeballs = new();
+    
+    // todo, this used vr HandSide. Had to exclude for server build. Make an enums folder and use our own
+    [SerializeField] public HandSide handSide;
+    private readonly Dictionary<BallType, DodgeballIndex> _dodgeballDictionary = new();
+    private HandPose _handPose;
+    private GameObject _currentDodgeball;
+    private bool _updatePose;
+    private Animator _anim;
+    private static readonly int _SGrabbing = Animator.StringToHash("Grabbing");
+    public Vector3 BallPosition => _currentDodgeball.transform.position;
+
+    public void Start()
+    {
+        foreach (var dodgeball in dodgeballs) _dodgeballDictionary[dodgeball.type] = dodgeball;
+        _anim.enabled = true;
+    }
+
+    private void Awake()
+    {
+        _anim = GetComponent<Animator>();
+        _anim.enabled = false;
+    }
+
+    private NetBallPossession _possession;
+
+    public void SetBallType(BallType type)
+    {
+        if (!_anim) return;
+
+        if (type == BallType.None) _possession = NetBallPossession.None;
+        else _possession = handSide == 0 ? NetBallPossession.LeftHand : NetBallPossession.RightHand;
+
+        if (_currentDodgeball && (type == BallType.None || _dodgeballDictionary[type].dodgeball != _currentDodgeball))
+            _currentDodgeball.SetActive(false);
+        if (type == BallType.None)
+        {
+            _currentDodgeball = null;
+            _anim.SetBool(_SGrabbing, false);
+            return;
+        }
+
+        _anim.SetBool(_SGrabbing, true);
+        _currentDodgeball = _dodgeballDictionary[type].dodgeball;
+        _currentDodgeball.SetActive(true);
+    }
+}
